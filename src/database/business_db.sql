ec6c5e841e89f8e99afc0654ad51c3c24bc3d073
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2023-05-20 01:17:57 +0700

    updated db

---
+++ src/database/business_db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE business_db;
+USE business_db;
+
+CREATE TABLE businesses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  location VARCHAR(255),
+  latitude FLOAT,
+  longitude FLOAT,
+  term VARCHAR(255),
+  radius INT,
+  categories VARCHAR(255),
+  locale VARCHAR(255),
+  price VARCHAR(255),
+  open_now BOOLEAN,
+  open_at INT,
+  attributes VARCHAR(255),
+  sort_by VARCHAR(255)
+);
